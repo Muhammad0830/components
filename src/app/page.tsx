66910356541339b32067eee002b04d9297a3f38c
Muhammad0830 <abduqayumovm1@gmail.com>
2025-09-30 22:25:15 +0500

    enhancements of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ import NextLink from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <ul className="flex flex-col gap-2 ">
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl mb-4">Components</h1>
+      <h3 className="text-xl ">
+        Click one of the links below to see the components in action
+      </h3>
+      <ul className="flex flex-col p-2 rounded-md border mt-4">
         <li>
           <NextLink
             href={"/dragAndDropCards"}
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 hover:bg-neutral-900 p-2 rounded-sm"
           >
             <Link className="w-5 h-5 text-black dark:text-white" />
             <div>Drag and Drop Cards</div>
