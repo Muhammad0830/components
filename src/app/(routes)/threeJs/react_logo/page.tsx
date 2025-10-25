9f7a2e9c621c726a5b214a20eae53768de6ae87a
Muhammad0830 <abduqayumovm1@gmail.com>
2025-10-25 15:20:36 +0500

    threeJs simple components added

---
+++ src/app/(routes)/threeJs/react_logo/page.tsx
@@ -0,0 +1,9 @@
+import ReactLogo3D from "@/components/three_js/ReactLogo3D";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black">
+      <ReactLogo3D />
+    </main>
+  );
+}
